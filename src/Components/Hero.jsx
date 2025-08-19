c6be9da19b846d9d7a6f3eabc48a0884d5ee34bb
PiyushK-stack <piyushkaithwas1501@gmail.com>
2025-08-19 12:08:16 +0530

    dell

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar.jsx'
-import First from '../Pages/First.jsx'
-import Second from '../Pages/Second.jsx'
-import Grocery from '../Pages/Grocery.jsx'
+import First from '../pages/First.jsx'
+import Second from '../pages/Second.jsx'
+import Grocery from '../pages/Grocery.jsx'
 
 const Hero = () => {
   return (
